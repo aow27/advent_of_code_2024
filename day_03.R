df38ec2fd597810f2795070b6c5cfd1afb9085f9
Alice Whiteway <alicewhiteway@gmail.com>
2024-12-07 10:09:32 +0000

    Completed day 2

---
+++ day_03.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+
+
+source('AoC functions.R')
+
+download_advent(2024,
+                3,
+                funct = readr::read_lines)
+input %>% 
+  as_tibble( ) %>% 
+  mutate(mults = str_extract_all(value, 
+                            'mul\\([0-9]{1,3},[0-9]{1,3}\\)')) %>% 
+  unnest_longer(mults, indices_to = 'col') %>% 
+  select(-value) %>% 
+  mutate(vals = str_extract_all(mults,'[0-9]+')) %>% 
+  unnest_wider(vals, names_sep = '_') %>% 
+  mutate(across(vals_1:vals_2,
+                as.numeric)) %>% 
+  summarise(sum(vals_1 * vals_2))
+
+# Part 2 ------------------------------------------------------------------
+
+input <- ("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+
+
+df <- input %>% 
+  as_tibble( ) %>% 
+  mutate(mults = str_extract_all(value, 
+                                 "mul\\([0-9]{1,3},[0-9]{1,3}\\)|don\\'t\\(\\)|do\\(\\)"),
+         row = row_number()) %>% 
+  unnest_longer(mults, indices_to = 'col') %>% 
+  mutate(enable_flag =case_when(mults == "don't()" ~ 0,
+                                mults == "do()"    ~ 1)) %>% 
+  # group_by(row) %>% 
+  fill(enable_flag) %>% 
+  # ungroup() %>% 
+  select(-value) %>% 
+  replace_na(list(enable_flag = 1)) %>%
+  filter(str_detect(mults, 
+                    "don\\'t\\(\\)|do\\(\\)", 
+                    negate = T)) %>% 
+  mutate(vals = str_extract_all(mults,'[0-9]+')) %>% 
+  unnest_wider(vals, names_sep = '_') %>% 
+  mutate(across(vals_1:vals_2,
+                as.numeric))  
+
+df %>% 
+  summarise(sum(vals_1 * vals_2 * enable_flag))
+
